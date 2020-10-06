8c3472972d345b63d6ef966491e9bdab2634b8d9
Your Name <sabrina.avila.09@gmail.com>
2020-10-05 22:49:45 -0700

    last update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,3 @@
-// @TODO: YOUR CODE HERE!
 var svgWidth = 960;
 var svgHeight = 500;
 
@@ -31,23 +30,20 @@ d3.csv("assets/data/data.csv").then(function(generalData) {
       data.poverty = +data.poverty;
     });
 
-    // Step 2: Create scale functions
-    // ==============================
+    // Create scale functions
     var xLinearScale = d3.scaleLinear()
       .domain([d3.min(generalData, d => d.income) -1000, d3.max(generalData, d => d.income)])
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(generalData, d => d.poverty)])
+      .domain([0, d3.max(generalData, d => d.poverty) + 5])
       .range([height, 0]);
 
-    // Step 3: Create axis functions
-    // ==============================
+    // Create axis functions
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
 
-    // Step 4: Append Axes to the chart
-    // ==============================
+    // Append Axes to the chart
     chartGroup.append("g")
       .attr("transform", `translate(0, ${height})`)
       .call(bottomAxis);
@@ -55,8 +51,7 @@ d3.csv("assets/data/data.csv").then(function(generalData) {
     chartGroup.append("g")
       .call(leftAxis);
 
-    // Step 5: Create Circles
-    // ==============================
+    // Create Circles
     var circlesGroup = chartGroup.selectAll("circle")
     .data(generalData)
     .enter()
@@ -64,24 +59,21 @@ d3.csv("assets/data/data.csv").then(function(generalData) {
     .attr("cx", d => xLinearScale(d.income))
     .attr("cy", d => yLinearScale(d.poverty))
     .attr("r", "15")
-    .attr("fill", "lightgreen")
+    .attr("fill", "lightblue")
     .attr("opacity", ".5");
 
-    var circlesGroup = chartGroup.selectAll("text")
+    var circlesText = chartGroup.selectAll(null)
     .data(generalData)
     .enter()
     .append("text")
     .attr("dx", d => xLinearScale(d.income))
     .attr("dy", d => yLinearScale(d.poverty))
     .attr("fill", "black")
-    // .attr("opacity", ".5")
-    // .attr("font-size", "15px")
     .attr("text-anchor", "middle")
-    .text(function(d){return d.abbr});
+    .text(function(d) {return d.abbr; });
 
 
-    // Step 6: Initialize tool tip
-    // ==============================
+    // Initialize tool tip
     var toolTip = d3.tip()
       .attr("class", "d3-tip")
       .offset([80, -60])
@@ -89,14 +81,15 @@ d3.csv("assets/data/data.csv").then(function(generalData) {
         return (`${d.state}<br>Income: ${d.income}<br>Poverty: ${d.poverty}`);
       });
 
-    // Step 7: Create tooltip in the chart
-    // ==============================
+    // Create tooltip in the chart
     chartGroup.call(toolTip);
 
-    // Step 8: Create event listeners to display and hide the tooltip
-    // ==============================
+    // Create event listeners to display and hide the tooltip
     circlesGroup.on("click", function(data) {
       toolTip.show(data, this);
+    })
+    circlesText.on("click", function(data){
+      toolTip.show(data,this);
     })
       // onmouseout event
       .on("mouseout", function(data, index) {
@@ -107,13 +100,13 @@ d3.csv("assets/data/data.csv").then(function(generalData) {
     chartGroup.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 0 - margin.left + 40)
-      .attr("x", 0 - (height / 2))
+      .attr("x", 0 - (height / 1.5))
       .attr("dy", "1em")
       .attr("class", "axisText")
       .text("Poverty");
 
     chartGroup.append("text")
-      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 40})`)
       .attr("class", "axisText")
       .text("Income");
   }).catch(function(error) {
